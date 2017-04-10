979f8d16c3ba0181b1551ea2457b43efce6e49ed
serenum <saveliev_@rambler.ru>
2017-04-10 22:24:18 +0600

    ShareActionProvider and Back button added

--- app/src/main/java/com/serenum/android/pizzorio/CreateOrderActivity.java
+++ app/src/main/java/com/serenum/android/pizzorio/CreateOrderActivity.java
@@ -1,7 +1,9 @@
 package com.serenum.android.pizzorio;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by Serenum on 10.04.2017.
@@ -13,5 +15,9 @@ public class CreateOrderActivity extends AppCompatActivity {
     public void onCreate(Bundle savedOnInstanceState) {
         super.onCreate(savedOnInstanceState);
         setContentView(R.layout.create_order);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/serenum/android/pizzorio/MainActivity.java
+++ app/src/main/java/com/serenum/android/pizzorio/MainActivity.java
@@ -2,12 +2,16 @@ package com.serenum.android.pizzorio;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    ShareActionProvider actionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        actionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setIntent("This is example text");
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -32,7 +39,15 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             case R.id.action_settings:
                 return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    private void setIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        actionProvider.setShareIntent(intent);
     }
 }
