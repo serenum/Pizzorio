69b9fc0d0a9f0b9a329530743709956d12c847ff
serenum <saveliev_@rambler.ru>
2017-04-10 20:45:55 +0600

    Create project structure and two activity

---
+++ app/src/main/java/com/serenum/android/pizzorio/CreateOrderActivity.java
@@ -0,0 +1,17 @@
+package com.serenum.android.pizzorio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Serenum on 10.04.2017.
+ */
+
+public class CreateOrderActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedOnInstanceState) {
+        super.onCreate(savedOnInstanceState);
+        setContentView(R.layout.create_order);
+    }
+}
---
+++ app/src/main/java/com/serenum/android/pizzorio/MainActivity.java
@@ -0,0 +1,38 @@
+package com.serenum.android.pizzorio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.create_order:
+                Intent intent = new Intent(this, CreateOrderActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
